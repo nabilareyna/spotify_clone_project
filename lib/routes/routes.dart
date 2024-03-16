0e2ccf3c2c1bb8aa77e55d082607bde94f733515
nabilareyn <nabilareynarahmadhani@gmail.com>
2024-03-16 23:59:32 +0700

    second commit

---
+++ lib/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:spotify_clone_project/modules/startPage/controller/c_start.dart';
+import 'package:spotify_clone_project/modules/startPage/ui/start.dart';
+
+class Routes {
+  static const String start = "/";
+
+  static List<GetPage> routes = [
+    GetPage(
+        name: start,
+        page: () => const StartPage(),
+        binding: BindingsBuilder(() {
+          Get.put(CStartPage());
+        }))
+  ];
+}
